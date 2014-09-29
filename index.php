6e044409543819a64ffe7659d47da15f7eff874a
Lewis Menelaws <lmenelaws112@gmail.com>
2014-09-28 22:01:40 -0400

    Got the list of Heroes to display on screen
    This took me FOREVER and I finally did it. Very early development but I
    am doing it!!!!!!

--- index.php
+++ index.php
@@ -14,21 +14,34 @@ $db_pass = "";
 $db_name = "heroes";
 
 //If it cant connect to your database or your server then this is why.
-@mysql_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to mySQL");
+$con = mysqli_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to mySQL");
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
-@mysql_select_db("$db_name") or die ("No database");
 
-echo"Successful Connection";
 
-//Holds string that is 
-$MaxIDQuery = "SELECT * FROM heroes;";
 
-//echo $MaxIDQuery;
+$result = mysqli_query($con, "SELECT name FROM heroes.heroes");
+if (!$result) {
+    die(mysqli_error($con));
+}
 
-$MaxIDResult = mysql_query($MaxIDQuery);
-$MaxArray = mysql_fetch_array($MaxIDResult);
+echo "<table border='1'>
+<tr>
+<th>Name</th>
+</tr>";
 
-echo $MaxIDResult;
+while($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td>" . $row['name'] . "</td>";
+}
+
+echo "</table>";
+
+
+
+mysqli_close($con);
 
 ?>
 <div id="container">
