918f8719ccd18d2ab14b8b52cb40416a2edfd69b
Lewis <lmenelaws112@gmail.com>
2014-09-29 23:08:55 -0400

    Got the Background video to work!

--- index.php
+++ index.php
@@ -78,12 +78,22 @@ mysqli_close($con);
 ?>
 
     <!-- Navigation -->
+
+    <script>
+    $(function() {
+        var BV = new $.BigVideo();
+        BV.init();
+        BV.show('img/dota2.mp4',{ambient:true});
+    });
+    
+    
+    </script>
     
 <a href="https://github.com/elebumm/heropicker"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
     <!-- Page Content -->
    <div class="wrapper">
     <div class="container">
-        <div class="row">
+        <div class="row" id="shit">
             
                 <h1 id="title">Hero Picker</h1>
                 <p id="slogan">Let me guess, you are about to find a match and you have no idea who to play. You dominated 2k MMR with Viper but are no longer able to snowball into late game. You have come to the right place my friend.</p>
@@ -110,20 +120,12 @@ mysqli_close($con);
 
 
     <!-- jQuery Version 1.11.0 -->
-    <script src="js/jquery-1.11.0.js"></script>
+   
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/jquery-ui.min.css"></script>
 
-	<script>
-	$(function() {
-	    var BV = new $.BigVideo();
-	    BV.init();
-	    BV.show('img/dota2.mp4',{ambient:true});
-	});
 	
-	
-	</script>
 </body>
 </html>
